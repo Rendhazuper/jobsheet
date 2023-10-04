f13215fe029ac525bc86d5a9c734703c1924bad8
Rendha <rendhazuper@gmail.com>
2023-10-04 09:14:45 +0700

    Pertemuan 6 Percobaan 2

---
+++ P6/Pemilihan2Percobaan224.java
@@ -0,0 +1,30 @@
+package P6;
+import java.util.*;
+public class Pemilihan2Percobaan224 {
+   
+
+    public static void main(String[] args) {
+    float sudut1, sudut2, sudut3, totalsudut;
+    Scanner input24 = new Scanner (System.in);
+    
+        System.out.print("masukkan sudut 1 :  ");
+        sudut1 = input24.nextFloat();  
+        System.out.print("masukkan sudut 2 :  ");
+        sudut2 = input24.nextFloat();  
+        System.out.print("masukkan sudut 3 :  ");
+        sudut3 = input24.nextFloat();  
+
+        totalsudut = sudut1 + sudut2 + sudut3;
+
+        if (totalsudut == 180) {
+            if ((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90)) {
+                System.out.println("Segitiga tersebut adalah segitiga siku-siku");
+                
+            }else
+            System.out.println("Segitiga tersebut adalah bukan segitiga siku-siku");
+            
+        }else
+        System.out.println("Bukan segitiga");
+    }
+    
+}
