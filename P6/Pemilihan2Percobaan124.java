8171ca017d8e847bc68ab46b4409dd833417f622
Rendha <rendhazuper@gmail.com>
2023-10-04 08:58:07 +0700

    modifikasi Pemilihan2Percobaan124

--- P6/Pemilihan2Percobaan124.java
+++ P6/Pemilihan2Percobaan124.java
@@ -1,24 +1,24 @@
 package P6;
-
 import java.util.*;
-/**
- * Pemilihan2Percobaan124
- */
 public class Pemilihan2Percobaan124 {
 
     public static void main(String[] args) {
-        int tahun ;
-        Scanner input25 = new Scanner (System.in);
+        int tahun;
+        Scanner input24 = new Scanner (System.in);
 
         System.out.println("masukkan tahun");
-        tahun = input25.nextInt();
+        tahun = input24.nextInt();
 
-        if ((tahun % 4) == 0 ) {
-            if ((tahun % 100) != 0 ){
+        if ((tahun % 4) == 0) {
+            if ((tahun % 400) == 0){
                 System.out.println("tahun kabisat");
             }else
             System.out.println("bukan tahun kabisat");
             
+        }else if ((tahun % 100) != 0){
+                System.out.println("tahun kabisat");
+        }else {
+            System.out.println("bukan tahun kabisat");
         }
 
 
