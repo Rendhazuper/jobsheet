e144bd2881a329e3872ad2f1c2f04bcf0f289b90
Rendha <rendhazuper@gmail.com>
2023-10-04 09:32:26 +0700

    Modifikasi Percobaan 2 Pertemuan 6

--- P6/Pemilihan2Percobaan224.java
+++ P6/Pemilihan2Percobaan224.java
@@ -11,7 +11,7 @@ public class Pemilihan2Percobaan224 {
         sudut1 = input24.nextFloat();  
         System.out.print("masukkan sudut 2 :  ");
         sudut2 = input24.nextFloat();  
-        System.out.print("masukkan sudut 3 :  ");
+        System.out.print("masukkan sudut 3 :  ");   
         sudut3 = input24.nextFloat();  
 
         totalsudut = sudut1 + sudut2 + sudut3;
@@ -20,8 +20,13 @@ public class Pemilihan2Percobaan224 {
             if ((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90)) {
                 System.out.println("Segitiga tersebut adalah segitiga siku-siku");
                 
-            }else
-            System.out.println("Segitiga tersebut adalah bukan segitiga siku-siku");
+            }else if ((sudut1 == 70)||(sudut2 == 70) || (sudut3 == 70)){
+                System.out.println("Segitiga tersebut adalah bukan segitiga sama kaki");
+            } else if ((sudut1 == 60)||(sudut2 == 60) || (sudut3 == 60)){
+                System.out.println("Segitiga tersebut adalah bukan segitiga sama sisi");
+              }else
+              System.out.println("segitiga sembarang");
+            
             
         }else
         System.out.println("Bukan segitiga");
