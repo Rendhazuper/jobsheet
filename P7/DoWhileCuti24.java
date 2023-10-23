87a334457ee774c81c79f2cabd82b5ba9da99cbd
Rendha <rendhazuper@gmail.com>
2023-10-24 02:49:15 +0700

    modifikasi percobaan 3

---
+++ P7/DoWhileCuti24.java
@@ -0,0 +1,38 @@
+    import java.util.*;
+    public class DoWhileCuti24 {
+        
+        public static void main(String[] args) {
+            
+            Scanner sc = new Scanner(System.in);
+
+            int jatahCuti, jumlahHari, kesempatan = 3;
+            String konfirmasi;
+
+            System.out.print("Jatah cuti: ");
+            jatahCuti = sc.nextInt();
+
+            do {
+                if (jatahCuti > 0) {
+                  System.out.println("apakah anda ingin mengambil cuti (y/t)? ");
+                konfirmasi = sc.next();
+                if (konfirmasi.equalsIgnoreCase("y")) {
+                    System.out.print("Jumlah hari: ");
+                    jumlahHari = sc.nextInt();
+                    if (jumlahHari <= jatahCuti) {
+                        jatahCuti -= jumlahHari;
+                        System.out.println("Sisa jatah cuti: "+ jatahCuti); 
+                    } else if (jumlahHari > jatahCuti) {
+                        kesempatan--;
+                        System.out.println("silahkan isikan sesuai jatah cuti");
+                        continue;
+                        
+                        
+                    }
+                } 
+                } else {
+                    break;
+                }
+                
+            } while (kesempatan  > 0);
+        }
+    }
