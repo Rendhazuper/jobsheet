5ba33e18d4abd008e8204c426e30df599b269827
Rendha <rendhazuper@gmail.com>
2023-10-24 00:10:37 +0700

    percobaan 1

---
+++ P7/ForKelipatan24.java
@@ -0,0 +1,28 @@
+/**
+ * ForKelipatan24
+ */
+import java.util.*;
+
+public class ForKelipatan24 {
+
+    public static void main(String[] args) {
+        int kelipatan,jumlah = 0 ,counter = 0;
+        int total= 0 ;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Masukkan bilangan kelipatan (1-9) : ");
+        kelipatan = scan.nextInt();
+
+        for (int i = 1; i <= 50; i++) {
+            if (i % kelipatan == 0 ) {
+                total += i ;
+                counter++;
+                System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
+                System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, total);
+                
+            }
+            
+        }
+    }
+}
