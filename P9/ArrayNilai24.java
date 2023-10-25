9d924a3dffb4f8fcad3ce39ca146d2a95b47ed82
Rendha <rendhazuper@gmail.com>
2023-10-25 10:37:04 +0700

    percobaan 2

---
+++ P9/ArrayNilai24.java
@@ -0,0 +1,22 @@
+package P9;
+
+import java.util.Scanner;
+
+public class ArrayNilai24 {
+    public static void main(String[] args) {
+                int [] nilaiAkhir = new int [10];
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            System.out.print("Masukkan nilai akhir ke-"+i+" : ");
+            nilaiAkhir[i] = sc.nextInt();
+            }  
+            for (int i = 0; i < nilaiAkhir.length; i++) {
+                if (nilaiAkhir[i] > 70){
+                    System.out.println("Mahasiswa ke-" +i+ " adalah lulus!");
+                }else if(nilaiAkhir[i] < 70)
+                System.out.println("Mahasiswa ke-"+i+ "tidak lulus!");
+
+}
+    }
+}
