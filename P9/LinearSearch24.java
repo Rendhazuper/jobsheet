d29074e6f7d2e89976befbe0ca21fdaedbe4e34c
Rendha <rendhazuper@gmail.com>
2023-10-25 11:29:44 +0700

    percobaan 4 modifikasi

--- P9/LinearSearch24.java
+++ P9/LinearSearch24.java
@@ -1,15 +1,31 @@
 package P9;
+import java.util.*;
 
 public class LinearSearch24 {
     public static void main(String[] args) {
-        int[] arrayint = {34,18,26,48,72,20,56, 63};
-        int key  = 20;
+        Scanner sc = new Scanner(System.in);
+        int arrayint ;
+        int key ;
         int hasil = 0; 
 
-        for (int i = 0; i < arrayint.length; i++) {
-            if(arrayint[i] == key ){
-                hasil = i;
+        System.out.println("Masukkan jumlah elemen array : ");
+        arrayint = sc.nextInt();
+        int [] elemen = new int[arrayint];
+
+        for (int i = 0; i < elemen.length; i++) {
+            System.out.println("masukkan elemen array ke-"+(i+1)+ ": ");
+            elemen[i] = sc.nextInt();
+
+        }
+        System.out.println("masukkan key yang ingin dicari : ");
+        key = sc.nextInt();
+
+        for (int i = 0; i < elemen.length; i++) {
+            if(elemen[i] == key ){
+                hasil = i+1;
                 break; 
+            }else{
+                System.out.println("key tidak ditemukan");
             }
         }
         System.out.println("key ada dalam array pada posisi indeks ke-"+hasil);
