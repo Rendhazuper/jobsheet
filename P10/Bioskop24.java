db8be39d738286afbdaeadc49b4bd17e390dfc00
Rendha <rendhazuper@gmail.com>
2023-11-01 07:31:01 +0700

    modifikasi 10 percobaan 1

--- P10/Bioskop24.java
+++ P10/Bioskop24.java
@@ -17,13 +17,19 @@ public class Bioskop24 {
         System.out.printf("%s \t %s \n", penonton[3][0], penonton[3][1]);
 
         System.out.println(penonton.length);
-        for (int i = 0; i<penonton.length;i++) {
-            System.out.println("Panjang dari baris ke-" + (i+1)+": "+penonton[1].length);
+        for (String[] barisPenonton : penonton) {
+            System.out.println("Panjang baris: " + barisPenonton.length);
+        }
+        System.out.println("Penonton pada baris ke-3: ");
+
+        for (int i = 0; i < penonton[2].length; i++) {
+            System.out.println(penonton[2][i]);
+        }
+    }
 
 
     }
-    }
-}
+
 
     
 
