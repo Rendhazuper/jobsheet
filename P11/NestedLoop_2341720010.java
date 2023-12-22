ba672ec7a3a8f1ea12aebd611eabc516a05cb009
Rendha <rendhazuper@gmail.com>
2023-12-22 15:25:44 +0700

    .

---
+++ P11/NestedLoop_2341720010.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class NestedLoop_2341720010 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double [][] temps = new double [5][7];
+
+        for (int i = 0 ; i < temps.length; i++) {
+            System.out.println("Kota ke-" + i);
+            for (int j = 0; j < temps[0].length; j++) {
+                    System.out.println("Hari ke-" + (j + 1) + " : ");
+                    temps[i][j] = sc.nextDouble();
+            }
+            System.out.println();
+        }
+        for (int i = 0 ; i < temps.length; i++) {
+            System.out.println("Kota ke-" + (i + 1) + " : ");
+            for (double temp : temps[i]) {
+                    System.out.println(temp + " ");
+            }
+            double sum = 0;
+            for (double temp : temps[i]) {
+                    sum += temp;
+            }
+            double average = sum / temps[i].length;
+            System.out.println("Rata-rata: " + average);
+            System.out.println();
+        }
+    }
+}
---
+++ P11/quiz.java
@@ -0,0 +1,31 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class quiz {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+
+        char menu = 'y';
+
+        do {
+            int number = random.nextInt(10) + 1;
+            boolean success = false;
+            do {
+                System.out.print("Tebak Angka (1 - 10) : ");
+                int answer = sc.nextInt();
+                sc.nextLine();
+                if (answer > number) {
+                    System.out.print("Angka yang Anda masukkan terlalu besar.");
+                } else if (answer < number) {
+                    System.out.print("Angka yang Anda masukkan terlalu kecil.");
+                } else {
+                    success = true;
+                }
+            } while (!success);
+            System.out.print("Jawaban Anda Benar");
+            System.out.print("Apakah anda ingin mengulang permainan (Y/t) ? ");
+            menu = sc.nextLine().charAt(0);
+        }while (menu == 'y' || menu == 'Y');
+    }
+}
